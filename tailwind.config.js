5655697bd09938e31da06e7e0e725fbc7e53eda6
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-20 00:29:10 +0000

    base project setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
