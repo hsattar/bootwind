7f41017069b92a8ea1f84c068b40a63760ae7f9b
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-20 01:52:02 +0000

    added homepage but tailwindcss not displaying properly

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,24 @@
+import { componentCards } from "../data/componentCards"
+import { Link } from 'react-router-dom'
+
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className="container mx-auto text-center font-mono capitalize">
+            <h1 className="mt-4 mb-3 text-5xl">BOOTWIND</h1>
+            <h2 className="text-2xl uppercase">Creating Bootstrap Components Using Tailwind CSS</h2>
+
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6">
+            {
+                componentCards.map(component => (
+                    <Link to={component.name} key={component.name}>
+                        <div className="border p-3 rounded-md shadow-md hover:bg-gray-900 hover:text-gray-100 cursor-pointer h-24 sm:h-32 md:h-36 mx-3">
+                            <p className="font-bold text-xl mb-2">{component.name}</p>
+                            <p>{component.description}</p>
+                        </div>
+                    </Link>
+                ))
+            }
+            </div>
+        </div>           
     )
 }
---
+++ src/data/componentCards.ts
@@ -0,0 +1,90 @@
+export const componentCards = [
+    {
+        name: "accordians",
+        description: "Accordians Can Be Used For FAQ's"
+    },
+    {
+        name: "alerts",
+        description: "Alerts Are Used To Warn The User About Something"
+    },
+    {
+        name: "badges",
+        description: "Badges Are Used To Display Extra Information To The User"
+    },
+    {
+        name: "breadcrumbs",
+        description: "Breadcrumbs Help The User navigate between pages"
+    },
+    {
+        name: "buttons",
+        description: "buttons are for the user to click which will trigger something"
+    },
+    {
+        name: "cards",
+        description: "cards are used to display information like this"
+    },
+    {
+        name: "carousels",
+        description: "carousels are a slideshow, most cases will involve images"
+    },
+    {
+        name: "collapse",
+        description: "a collapse is used to hide or display information"
+    },
+    {
+        name: "dropdowns",
+        description: "a dropdown will present the user with more options"
+    },
+    {
+        name: "forms",
+        description: "a form is used for the vistor to send a message to the owner"
+    },
+    {
+        name: "jumbotrons",
+        description: "a jumbotron is a box used to indicate text which requires extra attention"
+    },
+    {
+        name: "list groups",
+        description: "a collection of a group of items"
+    },
+    {
+        name: "modals",
+        description: "a custom pop up message displayed to the user"
+    },
+    {
+        name: "navbars",
+        description: "found at the top of the page showing the main pages of a website"
+    },
+    {
+        name: "navs & tabs",
+        description: "can be used to help seperate different sections of content"
+    },
+    {
+        name: "off canvas",
+        description: "add a hidden sidebar for extra navigation links"
+    },
+    {
+        name: "popovers",
+        description: "displays extra information when clicking an element"
+    },
+    {
+        name: "progress bars",
+        description: "helps indicate to the user that something is happening"
+    },
+    {
+        name: "skeleton placeholders",
+        description: "used to indicate some data is loading"
+    },
+    {
+        name: "spinners",
+        description: "helps indicate to the user that something is loading"
+    },
+    {
+        name: "tables",
+        description: "tables are used to show a group of related information"
+    },
+    {
+        name: "toasts",
+        description: "a small notification similar to mobile push notifications"
+    }
+]
